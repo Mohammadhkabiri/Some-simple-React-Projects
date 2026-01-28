835eb8a7c6eed47962f03b6147c61e50ce125f3d
Mohammad <m.kabiri.s1385@gmail.com>
2026-01-29 02:13:57 +0330

    initialize the webpage

---
+++ project/src/App.jsx
@@ -0,0 +1,46 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="../src/assets/dSW--react-core-concepts.png" />
+      <h1>
+        My React Project for practice
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, explicabo dolores facilis quod nisi consequatur reiciendis omnis nesciunt ab! Maxime voluptatem ipsa repellat vitae animi ullam ex facere praesentium ea.
+      </p>
+      </header>
+      <main>
+
+        <h2>
+          it,s time to get start
+        </h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// function App() {
+//   return (
+//     <div>
+//       <header>
+//         <img src="assets/react-core-concepts.png" alt="Stylized atom" />
+//         <h1>React Essentials</h1>
+//         <p>
+//           Fundamental React concepts you will need for almost any app you are
+//           going to build!
+//         </p>
+//       </header>
+
+//       <main>
+//         <h2>Time to get started!</h2>
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default App;
