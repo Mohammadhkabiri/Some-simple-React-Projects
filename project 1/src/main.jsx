a7e48955204f59d8f38426006bc2233674d690fa
Mohammad <m.kabiri.s1385@gmail.com>
2026-01-29 02:14:53 +0330

    Add files via upload

---
+++ project 1/src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./main.css";
+import "./App.css"
+const Root = document.getElementById("root");
+ReactDOM.createRoot(Root).render(<App />);
