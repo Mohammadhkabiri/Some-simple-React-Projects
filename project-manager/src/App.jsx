fbd50913072188ecdf75f71d82185548b5ace015
Mohammad <m.kabiri.s1385@gmail.com>
2026-02-09 06:02:55 +0330

    Add No Project Selected component and style that

--- project-manager/src/App.jsx
+++ project-manager/src/App.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "./components/Sidebar";
 import Newproject from "./components/Newproject";
+import NoProjectSelected from "./components/NoProjectSelected";
 function App() {
   return (
     <main className="my-8 h-screen flex gap-8">
       <Sidebar />
-      <Newproject />
+      <NoProjectSelected />
+
     </main>
   );
 }
---
+++ project-manager/src/components/NoProjectSelected.jsx
@@ -0,0 +1,22 @@
+import image from "../assets/no-projects.png";
+import Button from "./button";
+export default function NoProjectSelected() {
+  return (
+    <div className="mt-24 text-center w-2/3 ">
+      <img
+        src={image}
+        alt="Empty Task List"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a Project or get started with new one
+      </p>
+      <p className="mt-8">
+        <Button button={"Create New Project"} />
+      </p>
+    </div>
+  );
+}
