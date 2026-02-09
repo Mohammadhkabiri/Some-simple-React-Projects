df2dc26c998881dfcda1edc8bcab1ad69bca143d
Mohammad <m.kabiri.s1385@gmail.com>
2026-02-09 04:56:54 +0330

    Add new project structure beside Sidebar

--- project-manager/src/App.jsx
+++ project-manager/src/App.jsx
@@ -1,8 +1,10 @@
 import Sidebar from "./components/Sidebar";
+import Newproject from "./components/Newproject";
 function App() {
   return (
-    <main className="my-8 h-screen">
+    <main className="my-8 h-screen flex gap-8">
       <Sidebar />
+      <Newproject />
     </main>
   );
 }
